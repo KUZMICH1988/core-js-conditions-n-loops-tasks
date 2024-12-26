0676d580c618e876bc0b4b4f84254f83c5d36018
KuzmichAleksandr <fnt111333@gmail.com>
2024-12-27 01:10:24 +0300

    feat: solve 5 tasks

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -69,8 +69,14 @@ function getMaxNumber(a, b, c) {
  * {x: 1, y: 1}, {x: 2, y: 8} => false
  * {x: 1, y: 1}, {x: 2, y: 8} => false
  */
-function canQueenCaptureKing(/* queen, king */) {
-  throw new Error('Not implemented');
+function canQueenCaptureKing(queen, king) {
+  if (queen.x === king.x || queen.y === king.y) {
+    return true;
+  }
+  if (Math.abs(queen.x - king.x) === Math.abs(queen.y - king.y)) {
+    return true;
+  }
+  return false;
 }
 
 /**
@@ -91,8 +97,17 @@ function canQueenCaptureKing(/* queen, king */) {
  *  2, 2, 5   => false
  *  3, 0, 3   => false
  */
-function isIsoscelesTriangle(/* a, b, c */) {
-  throw new Error('Not implemented');
+function isIsoscelesTriangle(a, b, c) {
+  if (a === 0 || b === 0 || c === 0) {
+    return false;
+  }
+  if (a + b < c || b + c < a || c + a < b) {
+    return false;
+  }
+  if (a === b || a === c || b === c) {
+    return true;
+  }
+  return false;
 }
 
 /**
@@ -109,8 +124,28 @@ function isIsoscelesTriangle(/* a, b, c */) {
  *  10  => X
  *  26  => XXVI
  */
-function convertToRomanNumerals(/* num */) {
-  throw new Error('Not implemented');
+function convertToRomanNumerals(num) {
+  let roman = '';
+  let num1 = num;
+  while (num1 > 0) {
+    if (num1 >= 10) {
+      roman += 'X';
+      num1 -= 10;
+    } else if (num1 >= 9) {
+      roman += 'IX';
+      num1 -= 9;
+    } else if (num1 >= 5) {
+      roman += 'V';
+      num1 -= 5;
+    } else if (num1 >= 4) {
+      roman += 'IV';
+      num1 -= 4;
+    } else {
+      roman += 'I';
+      num1 -= 1;
+    }
+  }
+  return roman;
 }
 
 /**
